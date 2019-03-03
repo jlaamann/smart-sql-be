8ad1ac664f8410e7a8f20a0cc9be2fc631f5d17e
Johannes Laamann <jelaamann@gmail.com>
2019-03-03 17:01:15 +0200

    Setup

---
+++ src/main/java/com/jlaamann/smartsql/SmartsqlApplication.java
@@ -0,0 +1,13 @@
+package com.jlaamann.smartsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartsqlApplication.class, args);
+    }
+
+}
