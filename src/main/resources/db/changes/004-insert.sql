db8f953aa4a171766b801907ac69feea41955a21
Johannes Laamann <jelaamann@gmail.com>
2019-03-20 23:52:53 +0200

    Fix exercises for chapter 3

--- src/main/resources/db/changes/004-insert.sql
+++ src/main/resources/db/changes/004-insert.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 
---changeset johannes:0040-insert
+--changeset johannes:0041-insert
 insert into exercise (answer, chapter_id, question, test_query, type) values
-('SELECT * FROM film WHERE director LIKE ''J%'';',
+('SELECT director FROM film WHERE director LIKE ''J%'';',
  3,
  'Leia kõikide režissööride nimed (director) tabelist FILM, kelle eesnimi (director) algab tähega J',
  'SELECT director FROM film WHERE director LIKE ''J%'';',
